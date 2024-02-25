f8072320a8895ebef023785aefd7455744ff0aa8
RodrigoRPaulino <rodrigoramospaulino@gmail.com>
2024-02-25 14:20:10 -0300

    Adicionando Projeto Livraria e removendo Referencias

---
+++ ProfPaschoal/ProjetoLivraria/ControlLivraria/Domain/ListaDeLivros.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace ControlLivraria.Domain
+{
+    public class ListaDeLivros
+    {
+        List<Livros> list;
+
+        public ListaDeLivros()
+        {
+            list = new List<Livros>();
+        }
+
+        public void CadastraLivro(Livros livro)
+        {
+            list.Add(livro);
+        }
+
+        public List<Livros> ListaLivros()
+        {
+            return list;
+        }
+        public void SalvaLocalJSON(string arquivo)
+        {
+            string json = JsonSerializer.Serialize(list);
+            File.WriteAllText(arquivo, json);
+
+        }
+
+    }
+}
