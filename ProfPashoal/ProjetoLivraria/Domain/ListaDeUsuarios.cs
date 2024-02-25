0474fe7c6ab733970ada1a2ded7e86f359997e07
RodrigoRPaulino <rodrigoramospaulino@gmail.com>
2024-02-25 14:40:27 -0300

    Adicionando Projeto Livraria

---
+++ ProfPashoal/ProjetoLivraria/Domain/ListaDeUsuarios.cs
@@ -0,0 +1,102 @@
+﻿using System.Data;
+using System.Text;
+using System.Xml;
+using Newtonsoft.Json;
+using System.Linq;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+
+namespace ControlLivraria.Domain
+{
+    public class ListaDeUsuarios
+    {
+
+        List<Usuario> ListaUsuarios { get; set; }
+
+        public ListaDeUsuarios()
+        {
+            ListaUsuarios = new List<Usuario>();
+
+        }
+
+
+        public void AdicionaUsuarios(Usuario usuario)
+        {
+
+            ListaUsuarios.Add(usuario);
+        }
+
+
+
+        public List<Usuario> GetUsuarios()
+        {
+
+            return ListaUsuarios;
+        }
+
+        public void SalvaLocalJSON(string arquivo)
+        {
+            string json = JsonSerializer.Serialize(ListaUsuarios);
+            File.WriteAllText(arquivo, json);
+
+        }
+
+        public void SalvaLocalCSV(string arquivo)
+        {
+            string json = JsonSerializer.Serialize(ListaUsuarios);
+
+            try
+            {
+                XmlNode xml = JsonConvert.DeserializeXmlNode("{records:{record:" + json + "}}");
+
+                XmlDocument xmldoc = new XmlDocument(); xmldoc.LoadXml(xml.InnerXml);
+
+                DataSet dataSet = new DataSet(); dataSet.ReadXml(new XmlNodeReader(xmldoc));
+
+                string csv = DTableToCsv(dataSet.Tables[0], ",");
+
+                File.WriteAllText(arquivo, csv);
+                //return csv;
+
+            }
+            catch { throw; }
+
+
+        }
+
+        private string DTableToCsv(DataTable table, string delimator)
+        {
+            try
+            {
+                var result = new StringBuilder();
+
+                for (int i = 0; i < table.Columns.Count; i++)
+                {
+                    result.Append(table.Columns[i].ColumnName);
+                    result.Append(i == table.Columns.Count - 1 ? "\n" : delimator);
+                }
+
+                foreach (DataRow row in table.Rows)
+                    for (int i = 0; i < table.Columns.Count; i++)
+                    {
+                        result.Append(row[i].ToString());
+                        result.Append(i == table.Columns.Count - 1 ? "\n" : delimator);
+                    }
+
+                return result.ToString().TrimEnd(new char[] { '\r', '\n' });
+            }
+            catch { throw; }
+        }
+
+        public void CarregaLocal(string arquivo)
+        {
+
+            ListaUsuarios = File.ReadAllLines(arquivo)
+                                           .Skip(1)
+                                           .Select(v => Usuario.FromCsv(v))
+                                           .ToList();
+
+        }
+
+
+    }
+}
---
+++ ProfPashoal/ProjetoLivraria/Domain/Livros.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlLivraria.Domain
+{
+    public class Livros
+    {
+        /// <summary>
+        /// Nome do Livro
+        /// </summary>
+        public string Livro { get; set; }
+        /// <summary>
+        /// Quantidade de Páginas
+        /// </summary>
+        public int Paginas { get; set; }
+        /// <summary>
+        /// CODIGO ISBN (International Standard Book Number)
+        /// </summary>
+        public double Codigo { get; set; }
+        /// <summary>
+        /// Valor do Livro em Reais (BRL)
+        /// </summary>
+        public decimal Valor { get; set; }
+
+        /// <summary>
+        /// Construtor para Livro padrão 
+        /// </summary>
+        /// <param name="livro">Nome do Livro</param>
+        /// <param name="paginas">Numero de Paginas</param>
+        /// <param name="codigo">Código ISBN</param>
+        /// <param name="valor"> Preço (BRL)</param>
+        public Livros(string livro, int paginas, double codigo, decimal valor)
+        {
+            Livro = livro;
+
+            Paginas = paginas;
+
+            Codigo = codigo;
+
+            Valor = valor;
+        }
+        public Livros()
+        {
+
+        }
+
+
+
+
+        public override string ToString()
+        {
+            return Livro;
+        }
+
+
+
+
+
+
+
+    }
+}
